9cc42c871e5c9516356e96d1827e018ac63fe52a
Aino-Kaisa Piironen <ainopii@uef.fi>
2023-12-07 14:04:27 +0200

    Ready for Assignment 6

--- data/meet_and_repeat.R
+++ data/meet_and_repeat.R
@@ -26,103 +26,81 @@ summary(BPRS)
 # and the rest of the columns are longitudinal/repeated observations (results of the brief psychiatric rating scale, BPRS) of each week from baseline (0) to week 8.
 # To note, IDs run from 0 to 20 for both treatment groups but there are totally 40 different subjects.  
 
-# 1.4. Converting categorical variables to factors: treatment (1,2). 
+# 1.4. Converting categorical variables to factors: treatment (1,2), subject (1-20). 
 
 BPRS <- BPRS %>% 
-  mutate(treatment = factor(treatment)) # overwrite the original variable
+  mutate(treatment = factor(treatment),
+         subject = factor(subject)) # overwrite the original variable
 
 # 1.5. Converting data to long form. Adding a week variable to BPRS.
 # Need to get each variable is in its own column, and each observation is in its own row.
 # Variables: treatment, subject, week, BPRS (points)
 
-# changing column names for BPRS points in each week
-colnames(BPRS)[3:11] <- c(0:8) # now I will have week data as numbers in the tidy format (instead of "week0" etc.)
+BPRSL <-  pivot_longer(BPRS, cols = -c(treatment, subject),
+                       names_to = "weeks", values_to = "BPRS") %>%
+  arrange(weeks) #order by weeks variable
 
-# saving the columns with BPRS points
-weeks <- c("0", "1", "2", "3", "4", "5", "6", "7", "8")
+# Extract the week number
+BPRSL <-  BPRSL %>% 
+  mutate(week = as.integer(substr(weeks, 5,5)))
 
-# not the best and the shortest way to do this :)
+# Take a glimpse at the BPRSL data
+glimpse(BPRSL)
 
-BPRS_long <- BPRS %>% 
-  pivot_longer(all_of(weeks),
-               names_to = "week", 
-               values_to = "BPRS")
-
-str(BPRS_long)
-
-# changing week column from character to numeric
-BPRS_long$week <- as.numeric(BPRS_long$week)
-BPRS_long$subject %>%  as.integer()
-
-# Checking the data
-
-view(BPRS_long)
-str(BPRS_long)
 # Now the data has 4 columns, treatment and week as a factor and other (subject, BPRS) with numerical values.
 
-BPRS_long %>% group_by(treatment, week) %>% 
+BPRSL %>% group_by(treatment, week) %>% 
   summarise(mean(BPRS))
 
 # In treatment group 1, the mean BPRS results from baseline to 8 weeks range from 47 to 29.3.
 # In the treatment group 2, from 49 to 33.6, respectively.
 
 # Saving long format data
-write_csv(BPRS_long, "BPRS_tidy.csv")
+write_csv(BPRSL, "BPRS_tidy.csv")
 
 # PART 2. RATS data
 
 # 2.1 Reading the wide data, the data includes rownames
 
-rats <- read.table("https://raw.githubusercontent.com/KimmoVehkalahti/MABS/master/Examples/data/rats.txt", row.names = 1)
+RATS <- read.table("https://raw.githubusercontent.com/KimmoVehkalahti/MABS/master/Examples/data/rats.txt", row.names = 1)
 
 # 2.2 Saving the data to IODS project data folder (already as a working directory)
-write_csv(rats, "rats.csv")
+write_csv(RATS, "rats.csv")
 
 # 2.3 Exploring the data
-str(rats)
-colnames(rats)
-view(rats)
-summary(rats)
+str(RATS)
+colnames(RATS)
+view(RATS)
+summary(RATS)
 
 # rats data includes also longitudinal data from each subjects "ID" (n=16) in three groups.
 # There are 11 observations for each rats. Observations are body weights (g) of rats during a 9-week period. 
 
-# Converting categorical variables to factors: Group 1,2,3.
-rats <- rats %>% 
-  mutate(Group = factor(Group))
+# Converting categorical variables to factors: Group 1,2,3, and ID (1-16)
+RATS <- RATS %>% 
+  mutate(Group = factor(Group),
+         ID = factor(ID))
 
-# Converting data to long form. Adding a Time variable to RATS.
+RATSL <- pivot_longer(RATS, cols = -c(ID, Group), 
+                      names_to = "WD",
+                      values_to = "Weight") %>% 
+  mutate(Time = as.integer(substr(WD, 3, 4))) %>%
+  arrange(Time)
 
-# changing column names using: colnames(df) = gsub("pattern", replacement, x = colnames(df)
-colnames(rats) <- gsub("WD+", "", colnames(rats))
+# Glimpse the data
+glimpse(RATSL)
 
-Time <- dplyr::select(rats, -ID, -Group)
-keep <- colnames(Time)
+# Four variables: ID, Group (factors), Time (in days) and Weight (in grams)
 
-rats_long <- rats %>% 
-  pivot_longer(all_of(keep),
-               names_to = "Time",
-               values_to = "Weight")
-
-str(rats_long)
-# days are characters, otherwise okey. 
-
-rats_long$Time <- as.numeric(rats_long$Time) # not it is numeric
-
-# Checking the data again
-str(rats_long)
-
-# Four variables: ID, Group (as a factor), Time (in days, as a factor) and Weight
-
-rats_long %>% group_by(Group) %>% 
+RATSL %>% group_by(Group) %>% 
   summarise(mean(Weight))
 
 # Group 1: 264 g, group 2: 485g and group 3: 526 g - Seems to be group differences!
 
-rats_long %>% group_by(Time) %>% 
+RATSL %>% group_by(Time) %>% 
   summarise(mean(Weight))
 
 # Overall, the body weight seems to increase during the 9-week period as expected.
 
 # Save the long format data
-write_csv(rats_long, "rats.tidy.csv")
+write_csv(RATSL, "rats.tidy.csv")
